814d5e6a97b74565d34ff07ee7b54fa5b5613456
shashi kumar <shashi@lbbd.in>
2017-01-03 12:35:29 +0530

    question page

--- UGC_flow/ViewController.swift
+++ UGC_flow/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
+    @IBAction func didTapQuestion4Button(sender: UIButton) {
+        let sb = UIStoryboard.init(name: "Question", bundle: nil)
+        let contr = sb.instantiateViewControllerWithIdentifier("QuestionParentViewController") as! QuestionParentViewController
+        contr.initialIndex = 3
+        self.presentViewController(contr, animated: true, completion: nil)
+    }
+    @IBAction func didTapQuestion3Button(sender: UIButton) {
+        let sb = UIStoryboard.init(name: "Question", bundle: nil)
+        let contr = sb.instantiateViewControllerWithIdentifier("QuestionParentViewController") as! QuestionParentViewController
+        contr.initialIndex = 2
+        self.presentViewController(contr, animated: true, completion: nil)
+    }
+    @IBAction func didTapQuestion2Button(sender: UIButton) {
+        let sb = UIStoryboard.init(name: "Question", bundle: nil)
+        let contr = sb.instantiateViewControllerWithIdentifier("QuestionParentViewController") as! QuestionParentViewController
+        contr.initialIndex = 1
+        self.presentViewController(contr, animated: true, completion: nil)
+    }
+    @IBAction func didTapQuestion1Button(sender: UIButton) {
+        let sb = UIStoryboard.init(name: "Question", bundle: nil)
+        let contr = sb.instantiateViewControllerWithIdentifier("QuestionParentViewController") as! QuestionParentViewController
+        contr.initialIndex = 0
+        self.presentViewController(contr, animated: true, completion: nil)
+    }
 }
 
